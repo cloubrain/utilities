84be123f77dc74d69505cc028e10fa9435bbb1b7
Horia Margarit <horia@cloubrain.com>
2014-08-29 15:12:05 -0700

    Initial Commit

---
+++ bin/structure_package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm README.md 
+mkdir -p bin docs utilities/test
+touch .gitignore CHANGES.txt LICENSE.txt MANIFEST.in README.txt bin/structure_package.sh utilities/__init__.py utilities/test/__init__.py
+chmod -R o-rwx .[a-zA-Z]*
+chmod -R g-rwx .[a-zA-Z]*
+chmod -R o-rwx [a-zA-Z]*
+chmod -R g-rwx [a-zA-Z]*
+
